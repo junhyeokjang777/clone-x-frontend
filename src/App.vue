07426803d4517ed50d7a711c265261a92f78142c
junhyeok <junhyeokjang777@gmail.com>
2025-12-18 19:30:36 +0900

    #3 Init LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-<h4>hello world hello git</h4>
+  <div class="container">
+    <LoginPage />
+  </div>
+  
 </template>
 
 <script>
+import LoginPage from './views/LoginPage.vue'
 
 export default {
   name: 'App',
+  components: { LoginPage }
 }
 </script>
-
 <style>
+  body{
+    background-color: black;
+    color: white;
+  }
+  .container{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;  /*플렉스에서 colum은 생성물이 밑에 하나씩 생김 row로 하면 오른쪾으로 하나씩 생김 */
+    justify-content: center; /* 수직옵션end하면 가장아래, cneter하면 중간에 위치 */
+    align-items: center; /* 수평옵션으로 수평에서 가운데 왼쪽, 오른쪽 위치정함 */
 
+  }
 </style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" :placeholder="'email'" v-model="email">
+    <input type="password" :placeholder="'password'" v-model="password">
+
+    <button @click="login">로그인하기</button>
+    <div>
+        계정이 없으신가요?
+        <span>가입하기</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+        email: "",
+        password : "",
+    };
+  },
+  methods: {
+    login(){
+        console.log(this.email,this.password);
+    },
+  },
+};
+</script>
